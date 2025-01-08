157bbb6fd8d80cd44ba244c7f509fcbf289bce51
pragyanshu-kashyap <pragyanshukashayp30092001@gmail.com>
2025-01-08 13:57:16 +0530

    commented out router, since githubpages does'nt support it

--- src/App.js
+++ src/App.js
@@ -1,43 +1,41 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import About from './components/About';
-import React, {useState } from 'react';
-import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+// import About from './components/About';
+import React, { useState } from "react";
+import Alert from "./components/Alert";
+// import {
+// BrowserRouter as Router,
+//   Switch,
+//   Route
+// } from "react-router-dom";
 import Footer from './components/Footer';
 
- 
 function App() {
-  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [mode, setMode] = useState("light"); // Whether dark mode is enabled or not
   const [alert, setAlert] = useState(null); //to set alert whenever some operation occcurs
 
-  const showAlert = (message, type)=>{
-      setAlert({
-        msg: message,
-        type: type
-      })
-      setTimeout(() => {
-          setAlert(null);
-      }, 1500);
-  }
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
 
-  const toggleMode = ()=>{
-    if(mode === 'light'){
-      setMode('dark');
-      document.body.style.backgroundColor = '#042743';
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
       showAlert("Dark mode has been enabled !", "success");
-    }
-    else{
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
       showAlert("Light mode has been enabled !", "success");
     }
-  }
+  };
   return (
     /*<>
     <h1> this is me </h1>
@@ -60,26 +58,34 @@ function App() {
     </>*/
     // <div className="firstline"> Pragyanshu</div>
     //--------------> My customized project is here.<-------------------------- lecture 5
-
     <>
-    <Router>
-    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
-    <Alert alert={alert}/>
-    <div className="container my-3">
-    <Switch>
-    {/* /users --> Component 1      //since react uses partial matching thats why we are using "exact path" word instead of just path to avoid this conflict 
+      {/* <Router> */}
+      <Navbar
+        title="TextUtils"
+        mode={mode}
+        toggleMode={toggleMode}
+        key={new Date()}
+      />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        {/* <Switch> */}
+        {/* /users --> Component 1      //since react uses partial matching thats why we are using "exact path" word instead of just path to avoid this conflict 
         /users/home --> Component 2 */}
-          <Route exact path="/about">
+        {/*<Route exact path="/about">
             <About mode={mode} />
-          </Route>
-          <Route exact path="/">
-            <TextForm showAlert={showAlert} heading="Try TextUtils - One place for all your english operations " mode={mode}/>
-          </Route>
-    </Switch>
-    <Footer mode={mode} />
-    </div>
-    </Router>
-    </> 
+          </Route>*/}
+        {/* <Route exact path="/"> */}
+        <TextForm
+          showAlert={showAlert}
+          heading="Try TextUtils - One place for all your english operations "
+          mode={mode}
+        />
+        {/* </Route> */}
+        {/* </Switch> */}
+        <Footer mode={mode} />
+      </div>
+      {/* </Router> */}
+    </>
   );
 }
 
